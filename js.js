e98f24b7609cbc7d47fa85df9d3346930fa205a6
Omarhasanx21 <omar-hassan.2@hotmail.com>
2025-11-21 00:46:07 +0200

    Refactor calculator functionality and improve button handling

--- js.js
+++ js.js
@@ -1,9 +1,52 @@
-function overflowtest() {
-  let s = document.getElementById("texttest").value;
-  document.getElementById("ResultScreen").innerHTML = s;
-}
-
 //function to get element by id
 function getElement(v) {
   return document.getElementById(v);
+}
+const ResultScreen = getElement("ResultScreen");
+
+//array to store calculations
+let Cal = [];
+//calculator function
+function Calculator(c) {
+  switch (c) {
+    case "BackSpace":
+      Cal.pop();
+      DrawResult();
+      break;
+    case "Clear":
+      Cal = [];
+      DrawResult();
+      break;
+      case "_1" :
+        Cal.push(1);
+        DrawResult();
+        break;
+        case "_2" :
+          Cal.push(2);
+          DrawResult();
+          break;
+          case "Plus" :
+            Cal.push(+);
+            DrawResult();
+            break;
+            case "Equal" :
+              GetResult();
+              DrawResult();
+              break;
+  }
+}
+
+//function to draw result on screen
+function DrawResult() {
+  let tempResult;
+  for (let i = 0; i < Cal.length; i++) {
+    tempResult += Cal[i];
+  }
+  ResultScreen.innerHTMl = tempResult;
+  tempResult = "";
+}
+
+//calculat the result function
+function GetResult() {
+  ResultScreen.innerHTMl =eval(ResultScreen.innerHTMl);
 }
